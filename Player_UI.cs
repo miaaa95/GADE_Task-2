36281967ca3cf4243131d23983a7e5cce86ac413
miaaa95 <89992357+miaaa95@users.noreply.github.com>
2021-11-08 14:24:16 +0200

    Updated UI
    Monday

---
+++ Player_UI.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Task_1
+{
+    public partial class FrmHeroesGoblins : Form
+    {
+        public FrmHeroesGoblins()
+        {
+            InitializeComponent();
+        }
+
+        private void Player_UI_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bttnLeft_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
