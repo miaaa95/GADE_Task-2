ba7720679c636bb5107496a1ebddd1bf25aa1037
miaaa95 <89992357+miaaa95@users.noreply.github.com>
2021-11-10 23:08:21 +0200

    Finished Hero class

--- Task 2/Hero.cs
+++ Task 2/Hero.cs
@@ -4,11 +4,14 @@ using System.Text;
 
 namespace Task_1
 {
+    //Q2.6 concrete hero subclass that inherits from Character
     class Hero: Character
     {
         public Hero(int x, int y,TileType tile_type) :base(x, y, tile_type, 'H', 10, 10, 2,0)
         {
-
+            //constructor that receives x and y positions
+            //calls character constructor initialiser
+            //Hero = 2 Damage
         }
 
         bool checkvalidmove(movementEnum charactermove)
@@ -50,8 +53,10 @@ namespace Task_1
             return valid;
         }
         
+        //Overridden ReturnMove() method
         public override movementEnum ReturnMove( movementEnum move = movementEnum.NoMovement)
         {
+            //receives movement indicator based on directional button press
             if (checkvalidmove(move))
             {
                 return move;
@@ -59,6 +64,7 @@ namespace Task_1
             else return movementEnum.NoMovement;
         }
 
+        //overridden to string to display stats
         public override string ToString()
         {
             //Q3.2 Display the gold amount in the Stats display section
