bf42c1de8ae8cfe6976f0e2a54b3153a41a9ba3a
miaaa95 <89992357+miaaa95@users.noreply.github.com>
2021-11-10 22:35:46 +0200

    Completed Mage class
    finished

--- Task 2/Mage.cs
+++ Task 2/Mage.cs
@@ -4,18 +4,25 @@ using System.Text;
 
 namespace Task_1
 {
+    //Q2.3 concrete Mage subclass
+    //A new type of enemy
     class Mage : Enemy 
     {
         public Mage(int x, int y, TileType tile_type) : base(x, y, tile_type, 'M', 1, 5, 5)
         {
-
+             //constructor that receives x and y positions but takes variables through to Enemy class
+             //Mage = 5HP
+             //Mage = 5 damage
         }
 
-       public override bool checkrange(Character Target)
+        //overridden method called "checkrange"
+        public override bool checkrange(Character Target)
         {
-            
+            //Mages attack all characters within a one block radius all around them
+            //checking all ranges until it's own overridden CheckRange()
             if(Target.getX == X - 1 && Target.getY == Y - 1)
             {
+                //returns true if enemy is in a one block radius from the Mage's x and y position
                 return true;
             }
 
@@ -56,8 +63,11 @@ namespace Task_1
             }
         }
 
+        //overridden ReturnMove() method
         public override movementEnum ReturnMove(movementEnum move = movementEnum.NoMovement)
         {
+            //Always returns 0
+            //Mages never move
             return movementEnum.NoMovement;
         }
     }
