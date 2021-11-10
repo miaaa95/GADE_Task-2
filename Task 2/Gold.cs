5191b51cd2e23710856295454fb854f49296a9ef
miaaa95 <89992357+miaaa95@users.noreply.github.com>
2021-11-10 22:24:25 +0200

    Completed Gold class
    Finished

--- Task 2/Gold.cs
+++ Task 2/Gold.cs
@@ -4,11 +4,12 @@ using System.Text;
 
 namespace Task_1
 {
+    //Q2.2 Cocrete class called Gold inherited from Item
     class Gold: Item
     {
-
+        //private member variable for amount of gold represented through gold that is dropped
         private int amount;
-
+        //public accessor that can return the amount of gold
         public int Getamount
         {
             get { return amount; }
@@ -17,8 +18,10 @@ namespace Task_1
 
         private Random rnd = new Random();
 
+        //constructor receiving x and y positions that goes to the Item construcor through a constructor initialiser
         public Gold(int x, int y, TileType tile_type) : base(x, y, tile_type, '$')
         {
+            //sets amount to a random amount between 1 and 5 inclusively
             amount = rnd.Next(1,6);
         }
 
